447f0ea114517b86a94124aedceb01e832fe8f3b
Elena Ovcharenko <elen_rosee@MacBook-Pro-Ovcarenko.local>
2021-08-09 20:56:45 +0300

    start

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./ImageGalleryItem.scss";
+
+const ImageGalleryItem = () => (
+  <React.Fragment>
+    <li className="ImageGalleryItem">
+      <img src="" alt="" className="ImageGalleryItem-image" />
+    </li>
+    <li className="ImageGalleryItem">
+      <img src="" alt="" className="ImageGalleryItem-image" />
+    </li>
+    <li className="ImageGalleryItem">
+      <img src="" alt="" className="ImageGalleryItem-image" />
+    </li>
+    <li className="ImageGalleryItem">
+      <img src="" alt="" className="ImageGalleryItem-image" />
+    </li>
+    <li className="ImageGalleryItem">
+      <img src="" alt="" className="ImageGalleryItem-image" />
+    </li>
+    <li className="ImageGalleryItem">
+      <img src="" alt="" className="ImageGalleryItem-image" />
+    </li>
+    <li className="ImageGalleryItem">
+      <img src="" alt="" className="ImageGalleryItem-image" />
+    </li>
+    <li className="ImageGalleryItem">
+      <img src="" alt="" className="ImageGalleryItem-image" />
+    </li>
+    <li className="ImageGalleryItem">
+      <img src="" alt="" className="ImageGalleryItem-image" />
+    </li>
+    <li className="ImageGalleryItem">
+      <img src="" alt="" className="ImageGalleryItem-image" />
+    </li>
+    <li className="ImageGalleryItem">
+      <img src="" alt="" className="ImageGalleryItem-image" />
+    </li>
+    <li className="ImageGalleryItem">
+      <img src="" alt="" className="ImageGalleryItem-image" />
+    </li>
+  </React.Fragment>
+);
+
+export default ImageGalleryItem;
---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,21 @@
+import "./Searchbar.scss";
+
+const Searchbar = () => (
+  <header className="Searchbar">
+    <form className="SearchForm">
+      <button type="submit" className="SearchForm-button">
+        <span className="SearchForm-button-label">Search</span>
+      </button>
+
+      <input
+        className="SearchForm-input"
+        type="text"
+        autoComplete="off"
+        autoFocus
+        placeholder="Search images and photos"
+      />
+    </form>
+  </header>
+);
+
+export default Searchbar;
