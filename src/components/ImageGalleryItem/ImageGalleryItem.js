0022a8b9a34f21f6fdd466417dc60928f542e017
Elena Ovcharenko <elen_rosee@MacBook-Pro-Ovcarenko.local>
2021-08-17 01:19:19 +0300

    done

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,45 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import "./ImageGalleryItem.scss";
+import uuid from "react-uuid";
 
-const ImageGalleryItem = () => (
-  <React.Fragment>
-    <li className="ImageGalleryItem">
-      <img src="" alt="" className="ImageGalleryItem-image" />
-    </li>
-    <li className="ImageGalleryItem">
-      <img src="" alt="" className="ImageGalleryItem-image" />
-    </li>
-    <li className="ImageGalleryItem">
-      <img src="" alt="" className="ImageGalleryItem-image" />
-    </li>
-    <li className="ImageGalleryItem">
-      <img src="" alt="" className="ImageGalleryItem-image" />
-    </li>
-    <li className="ImageGalleryItem">
-      <img src="" alt="" className="ImageGalleryItem-image" />
-    </li>
-    <li className="ImageGalleryItem">
-      <img src="" alt="" className="ImageGalleryItem-image" />
-    </li>
-    <li className="ImageGalleryItem">
-      <img src="" alt="" className="ImageGalleryItem-image" />
-    </li>
-    <li className="ImageGalleryItem">
-      <img src="" alt="" className="ImageGalleryItem-image" />
-    </li>
-    <li className="ImageGalleryItem">
-      <img src="" alt="" className="ImageGalleryItem-image" />
-    </li>
-    <li className="ImageGalleryItem">
-      <img src="" alt="" className="ImageGalleryItem-image" />
-    </li>
-    <li className="ImageGalleryItem">
-      <img src="" alt="" className="ImageGalleryItem-image" />
-    </li>
-    <li className="ImageGalleryItem">
-      <img src="" alt="" className="ImageGalleryItem-image" />
-    </li>
-  </React.Fragment>
-);
+class ImageGalleryItem extends Component {
+  render() {
+    const { images, openModal } = this.props;
+    return (
+      <React.Fragment>
+        {images.map(({ webformatURL, largeImageURL, tags }) => (
+          <li key={uuid()} className="ImageGalleryItem">
+            <img
+              onClick={() => openModal(largeImageURL, tags)}
+              src={webformatURL}
+              alt={tags}
+              className="ImageGalleryItem-image"
+            />
+          </li>
+        ))}
+      </React.Fragment>
+    );
+  }
+}
+
+ImageGalleryItem.propTypes = {
+  openModal: PropTypes.func.isRequired,
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      webformatURL: PropTypes.string.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
+      tags: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
 
 export default ImageGalleryItem;
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,21 +1,54 @@
+import { Component } from "react";
+import PropTypes from "prop-types";
 import "./Searchbar.scss";
 
-const Searchbar = () => (
-  <header className="Searchbar">
-    <form className="SearchForm">
-      <button type="submit" className="SearchForm-button">
-        <span className="SearchForm-button-label">Search</span>
-      </button>
+class Searchbar extends Component {
+  state = {
+    request: "",
+  };
 
-      <input
-        className="SearchForm-input"
-        type="text"
-        autoComplete="off"
-        autoFocus
-        placeholder="Search images and photos"
-      />
-    </form>
-  </header>
-);
+  handleInputChangeEvent = (event) => {
+    const { name, value } = event.currentTarget;
+
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ request: "" });
+  };
+
+  render() {
+    return (
+      <header className="Searchbar">
+        <form onSubmit={this.handleSubmit} className="SearchForm">
+          <button type="submit" className="SearchForm-button">
+            <span className="SearchForm-button-label">Search</span>
+          </button>
+
+          <input
+            className="SearchForm-input"
+            type="text"
+            name="request"
+            autoComplete="off"
+            onChange={this.handleInputChangeEvent}
+            value={this.state.request}
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
 
 export default Searchbar;
